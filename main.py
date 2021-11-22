c66a5d97a0ebf760b7a009f74df2cfd92fbcbd44
tjonas005 <jonasroels6@gmail.com>
2021-11-22 14:12:22 +0100

    eerste werkende versie

--- main.py
+++ main.py
@@ -1,23 +1,68 @@
 from scapy.all import *
 
+nameDirectory = []
+IPDirectory = []
+DNSBACKUP = '10.99.6.1'
+
+def getSiteIP(siteName):
+    global nameDirectory
+    global IPDirectory
+    siteIP = ""
+    found = False
+
+    for i in range(len(nameDirectory)):
+        if nameDirectory[i] == siteName:
+            siteIP = IPDirectory[i]
+            found = True
+
+    if not(found):
+        networkLayer = IP(dst=DNSBACKUP)
+        transportLayer = UDP(dport=53, sport=56980)
+        applicationLayer = DNS(id=0x6000, rd=1, qd=DNSQR(qname=siteName, qtype= 'A'))
+
+        DNSQuery = networkLayer/transportLayer/applicationLayer
+
+        DNSrespone = sr1(DNSQuery)
+
+        siteIP = DNSrespone[DNS].an.rdata
+
+        nameDirectory.append(siteName)
+        IPDirectory.append(siteIP)
+
+    return(siteIP)
+
+
+
 def main():
     running = True
 
-    #stel asynchrone sniffer in op port 53 (standaard poort voor dns queries)
-    t = AsyncSniffer(filter = "port 53")
-
+    #stel asynchrone sniffer in op port 53 (standaard poort voor dns querie
+    t = AsyncSniffer(count=1, filter = "port 53")
     #maak een lege resulst lijst aan
     results = []
-
+    count = 0
+    hostIP = get_if_addr(conf.iface)
     #zolang de dns server runt blijf loopen
     while(running):
-
+        count += 1
         # start de sniffer
         t.start()
+        t.join()
 
         #loop daar alle gecapturde packets
         for packet in results:
-            print(packet)
+            if packet[IP].dst == hostIP and packet[DNS].qr == 0:
+                print("generating response", packet[IP].src)
+                siteName = packet[DNS].qd.qname
+                networkLayer = IP(dst=packet[IP].src)
+                transportLayer = UDP(dport=packet[UDP].sport, sport=53)
+                siteIP = getSiteIP(siteName)
+                applicationLayer = DNS(id=packet[DNS].id, aa=1, qr=1, rd=packet[DNS].rd, qdcount=packet[DNS].qdcount, qd=packet[DNS].qd, ancount=1, an=DNSRR(rrname=packet[DNS].qd.qname, type='A', ttl=1, rdata=siteIP))
+
+                DNSAnswer = networkLayer/transportLayer/applicationLayer
+                dnsResponse = sr1(DNSAnswer, timeout=1)
+
+
 
             # als packet een dns request is kijk of overeenkomstig ip in geheugen zit
 
@@ -32,7 +77,7 @@ def main():
                 # voeg waarde toe aan TLD server en return naar de client
 
         #stop de sniffer en update de results
-        results = t.stop()
+        results = t.results
 
 
 
